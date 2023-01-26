0558af693372917a514b83b7f612b7b8ba38f306
JelisM <jelismaiara@gmail.com>
2023-01-25 22:45:38 -0500

    fixing ejs

--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const commentsCtrl = require('../controllers/comments')
 const ensureLoggedIn = require('../config/ensureLoggedIn') 
 
-router.post('/recipes/:id/reviews', ensureLoggedIn, commentsCtrl.create)
+router.post('/recipes/:id/comments', ensureLoggedIn, commentsCtrl.create)
 router.delete('/comments/:id', ensureLoggedIn, commentsCtrl.delete)
 
 
